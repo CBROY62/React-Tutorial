172f4fabc13703942bbe4642b1406322f0a9234d
CBROY62 <chandrabhushankumar27080@gmail.com>
2025-02-02 17:55:15 +0530

    create a new project bharat clock app

---
+++ BharatClock/bharatclock/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Clockheding from './components/ClockHeding'
+import Clocksologan from './components/ClockSlogan'
+import CurrentTime from './components/CurrentTime'
+
+function App() {
+ 
+  return (
+    <center>
+      <Clockheding></Clockheding>
+      <Clocksologan></Clocksologan>
+      <CurrentTime></CurrentTime>
+    </center>
+  )
+}
+
+export default App
---
+++ BharatClock/bharatclock/src/components/ClockSlogan.jsx
@@ -0,0 +1,7 @@
+function Clocksologan() {
+    return(
+       <p className="lead">This is a the clock show the time in the bharat at</p>
+    )
+}
+
+export default Clocksologan

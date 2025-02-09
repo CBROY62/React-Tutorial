9f2717d6d525c37b56e24c4134d0c47f8e376d84
CBROY62 <chandrabhushankumar27080@gmail.com>
2025-02-09 16:00:03 +0530

    add new function

--- ContextUseHooks/contaxt/src/App.jsx
+++ ContextUseHooks/contaxt/src/App.jsx
@@ -2,14 +2,24 @@ import "./App.css";
 import ChildA from "./components/ChildA";
 import { createContext, useState } from "react";
 
-  export const UserContaxt = createContext();
+export const UserContaxt = createContext();
+
+export const ThemeContext = createContext();
 function App() {
-  const [user , setUser] = useState({name:"bhushan"});
+  const [user, setUser] = useState({ name: "bhushan" });
+
+  const [Theme, setTheme] = useState("Light");
   return (
     <>
-      <UserContaxt.Provider value= {user}>
-        <h1> hello </h1>
-        <ChildA></ChildA>
+      <UserContaxt.Provider value={user}>
+        <ThemeContext.Provider value={{ Theme, setTheme }}>
+          <div
+            className="contaneir"
+            style={{ backgroundColor: Theme ==='light' ?'beige' :'black' }}
+          >
+            <ChildA></ChildA>
+          </div>
+        </ThemeContext.Provider>
       </UserContaxt.Provider>
     </>
   );
--- ContextUseHooks/contaxt/src/components/ChildC.jsx
+++ ContextUseHooks/contaxt/src/components/ChildC.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { useContext } from 'react'
-import { UserContaxt } from '../App'
+import { UserContaxt , ThemeContext } from '../App'
 const ChildC = () => {
     const user = useContext(UserContaxt);
+    const {Theme , setTheme} = useContext(ThemeContext)
+
+    const handleClickButton = ()=>{
+      if (Theme === 'light') {
+        setTheme('dark')
+      }else{
+        setTheme('light');
+      }
+    }
   return (
     <div>
        <h1>final data {user.name}</h1> 
+       <button className='button-sectin' onClick={handleClickButton}>
+          Change Color
+       </button>
     </div>
   )
 }
